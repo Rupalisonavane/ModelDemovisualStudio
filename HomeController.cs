a3b8131ef9d04f7da7717ae292c2b614a7f14e3e
Rupalisonavane <73071731+Rupalisonavane@users.noreply.github.com>
2021-07-10 15:42:39 +0530

    Add files via upload
    to execute the above project first create mvc empty project in model folder add employee.cs file and add Index.cshtml in view folder in controller folder ad Home conntroller

---
+++ HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication7.Models;
+namespace WebApplication7.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var data = GetEmployee();
+            return View(data);
+        }
+        private Employee GetEmployee()
+        {
+            return new Employee()
+            {
+                Id = 1, Name = "Rupali", Address = "India"
+
+
+        };
+        }
+    }
+}
